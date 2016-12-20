65d8d929e3407c25a2ea3e2f77eb2759f246f78c
Jeremy Keith <jeremy@adactio.com>
2016-12-20 21:21:14 +0000

    Add link to .mobi version

--- serviceworker.js
+++ serviceworker.js
@@ -3,7 +3,7 @@
 // Import Jake's polyfill for async waitUntil
 importScripts('/js/async-waituntil.js');
 
-const version = 'v0.030::';
+const version = 'v0.031::';
 const staticCacheName = version + 'static';
 
 function updateStaticCache() {

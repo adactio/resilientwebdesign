8a97308fe7498ea054a0b36349c17f0bd05c1ac1
Jeremy Keith <jeremy@adactio.com>
2017-01-03 18:01:18 +0000

    Fix typo

--- serviceworker.js
+++ serviceworker.js
@@ -3,7 +3,7 @@
 // Import Jake's polyfill for async waitUntil
 importScripts('/js/async-waituntil.js');
 
-const version = 'v0.034::';
+const version = 'v0.035::';
 const staticCacheName = version + 'static';
 
 function updateStaticCache() {

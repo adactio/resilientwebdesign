b2ae6a0e23ac4b2af2ead924231710940335ddce
Jeremy Keith <jeremy@adactio.com>
2017-01-08 16:37:25 +0000

    Link to newest podcast episode (chapter 3)

--- serviceworker.js
+++ serviceworker.js
@@ -3,7 +3,7 @@
 // Import Jake's polyfill for async waitUntil
 importScripts('/js/async-waituntil.js');
 
-const version = 'v0.035::';
+const version = 'v0.036::';
 const staticCacheName = version + 'static';
 
 function updateStaticCache() {

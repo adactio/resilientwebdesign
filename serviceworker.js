6aca74e8f77a19fe8353d3df3cf7f0a5886a820d
Jeremy Keith <jeremy@adactio.com>
2017-02-09 16:49:14 -0500

    Don't cache offline image from service worker

--- serviceworker.js
+++ serviceworker.js
@@ -178,7 +178,7 @@ self.addEventListener('fetch', event => {
                 // OFFLINE
                 // If the request is for an image, show an offline placeholder
                 if (request.headers.get('Accept').indexOf('image') !== -1) {
-                    return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', {headers: {'Content-Type': 'image/svg+xml'}});
+                    return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', {headers: {'Content-Type': 'image/svg+xml', 'Cache-Control': 'no-store'}});
                 }
                 // If the request is for a page, show an offline message
                 if (request.headers.get('Accept').indexOf('text/html') !== -1) {

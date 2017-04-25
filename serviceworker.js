95e4cfdfe30a2a4c8b838de0ea50823b8d9ade1b
Jeremy Keith <jeremy@adactio.com>
2017-04-25 18:21:59 +0100

    Fix service worker

--- serviceworker.js
+++ serviceworker.js
@@ -159,32 +159,41 @@ self.addEventListener('fetch', event => {
                 );
                 return responseFromCache;
             }
-            // NETWORK
-            // If the file wasn't in the cache, make a network request
-            fetch(request)
-            .then( responseFromFetch => {
-                // Stash a fresh copy in the cache in the background
-                // (using the async waitUntil polyfill)
-                let responseCopy = responseFromFetch.clone();
-                event.waitUntil(
-                    caches.open(staticCacheName)
-                    .then( cache => {
-                        cache.put(request, responseCopy);
-                    })
+        })
+        ||
+        // NETWORK
+        // If the file wasn't in the cache, make a network request
+        fetch(request)
+        .then( responseFromFetch => {
+            // Stash a fresh copy in the cache in the background
+            // (using the async waitUntil polyfill)
+            let responseCopy = responseFromFetch.clone();
+            event.waitUntil(
+                caches.open(staticCacheName)
+                .then( cache => {
+                    cache.put(request, responseCopy);
+                })
+            );
+            return responseFromFetch;
+        })
+        .catch( () => {
+            // OFFLINE
+            // If the request is for an image, show an offline placeholder
+            if (request.headers.get('Accept').includes('image')) {
+                return new Response(
+                    '<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>',
+                    {
+                        headers: {
+                            'Content-Type': 'image/svg+xml',
+                            'Cache-Control': 'no-store'
+                        }
+                    }
                 );
-                return responseFromFetch;
-            })
-            .catch( () => {
-                // OFFLINE
-                // If the request is for an image, show an offline placeholder
-                if (request.headers.get('Accept').indexOf('image') !== -1) {
-                    return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', {headers: {'Content-Type': 'image/svg+xml', 'Cache-Control': 'no-store'}});
-                }
-                // If the request is for a page, show an offline message
-                if (request.headers.get('Accept').indexOf('text/html') !== -1) {
-                    return caches.match('/offline/');
-                }
-            });
+            }
+            // If the request is for a page, show an offline message
+            if (request.headers.get('Accept').includes('text/html')) {
+                return caches.match('/offline/');
+            }
         })
     );
 });
